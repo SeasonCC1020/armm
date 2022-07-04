ea55bcbd03d9d64633650e6b68608c7f4c4dc418
SeasonCC <user@DESKTOP-1NV2J5P>
2022-07-04 22:50:12 +0800

    magic

--- src/main/java/net/mcreator/armm/item/MagicStoneblueItem.java
+++ src/main/java/net/mcreator/armm/item/MagicStoneblueItem.java
@@ -1,21 +1,16 @@
 
 package net.mcreator.armm.item;
 
-import net.minecraft.world.level.Level;
 import net.minecraft.world.item.UseAnim;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.Component;
 
-import java.util.List;
+import net.mcreator.armm.init.ArmmModTabs;
 
 public class MagicStoneblueItem extends Item {
 	public MagicStoneblueItem() {
-		super(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(64).rarity(Rarity.COMMON));
+		super(new Item.Properties().tab(ArmmModTabs.TAB_ARMM).stacksTo(1).rarity(Rarity.COMMON));
 	}
 
 	@Override
@@ -26,11 +21,5 @@ public class MagicStoneblueItem extends Item {
 	@Override
 	public int getUseDuration(ItemStack itemstack) {
 		return 0;
-	}
-
-	@Override
-	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
-		super.appendHoverText(itemstack, world, list, flag);
-		list.add(new TextComponent("magic.stone"));
 	}
 }
